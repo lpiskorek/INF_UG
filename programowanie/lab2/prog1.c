598458bbca06e7416d390c3b6754e8ba3f1edf21
Łukasz Piskorek <lpiskorek@sigma.inf.ug.edu.pl>
2012-11-05 20:42:04 +0100

    petla

---
+++ programowanie/lab2/prog1.c
@@ -0,0 +1,22 @@
+/* Zad. 1 | Program wypisuje kolejne potęgi 2 nie przekraczające Łukasz Piskorek | 202543 | */
+
+#include <stdio.h>
+
+main()
+
+{
+  int x=2;
+
+  while (x < 2010){
+    printf("%d\n", x);
+    x*=2;
+
+  }
+}
+
+/*
+ALTERNATYWNE ROZWIAZANIE!!
+
+for (x=2;x < 2010; x*=2){
+printf("%d\n", x);
+*/
