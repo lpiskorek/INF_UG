a3cc16c0bcc9b15c6c6109b7949069ca4126506b
Łukasz Piskorek <lpiskorek@sigma.inf.ug.edu.pl>
2012-11-05 20:34:39 +0100

    program3

---
+++ programowanie/lab1/prog3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+  int a;
+  int b;
+  int s;
+  
+  scanf("%d", &a);
+    
+  b = 2;
+  s = a * b;
+  printf("%d", s);
+  printf("\n\n");
+  return 0;
+}
