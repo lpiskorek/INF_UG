589d23d9707c1f6af37e3b41ddfd4295ff0d8dbf
Łukasz Piskorek <lpiskorek@sigma.inf.ug.edu.pl>
2012-11-06 19:09:02 +0100

    delete

---
+++ systemy/lab1/odpowidz.sh
@@ -0,0 +1,14 @@
+## Bash i AWK - Petle/case - zadanie 3
+
+#!/bin/bash
+
+echo 'Czy jesteś człowiekiem?'
+read raz
+
+case $raz in
+        [tT][aA][kK]|[tT]) echo 'Twoja odp. to Tak' ;;
+        [nN][iI][eE]|[nN]) echo 'Twoja odp. to Nie' ;;
+        *) echo 'Nic nie wpisałeś'
+esac
+
+
